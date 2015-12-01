f95aa8876d9950b565092ae2e98b9b6295ac9375
Suryansh Rastogi <suryansh@gmail.com>
2015-12-01 00:06:08 +0000

    more stats outputteded from cross validation

--- crossValidation.m
+++ crossValidation.m
@@ -1,6 +1,6 @@
+% Cross validation for the parameter estimation
+
 function [] = crossValidation(k, x, y)
-    % vs contains the validation set start indexes (t for test)
-    % ve contains the validation set end indexes (t for test)
    
     % Transpose values into expected format
     [x2, y2] = ANNdata(x, y);
@@ -9,9 +9,15 @@ function [] = crossValidation(k, x, y)
     P = x2;
     % T represents the targets
     T = y2;
-
+    
+    % vs contains the validation set start indexes (t for test)
+    % ve contains the validation set end indexes (t for test)    
     [vs1,ve1, vs2, ve2, ts, te] = crossValidationIndexes(k, x, y);
+    
+    % saves nets for the iterations
     nets = cell(k,1);
+    % saves best training record for the  net
+    tr = cell(k,1);
     
     for i = 1:k
         % To create a network with one hidden layer and five neurons 
@@ -30,11 +36,21 @@ function [] = crossValidation(k, x, y)
         disp(st);
         st = sprintf('ValInd: %d:%d', ts{i}, te{i});
         disp(st);
-        
 
         % To train the ntwork for 100 epochs, and plot the output
         net.trainParam.epochs = 100;
-        nets{i} = train(net, P, T);
+        
+        [nets{i}, tr{i}] = traingd(net, P, T);
+
+        % Best epoch
+        disp(tr{i}.lr);
+
+        % Best performance on training set
+        disp(tr{i}.best_perf);
+
+        % Best performance on validation set
+        disp(tr{i}.best_vperf);
+
     end
 
 end
