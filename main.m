ddcae529c4942daf82c114439337cf0a71f0cb3e
Surya Rastogi <sr3213@ic.ac.uk>
2015-12-02 20:00:20 +0000

    Files

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 % Load x and y from dataset
-load('./forstudents/cleandata_students.mat');
+%load('./forstudents/cleandata_students.mat');
 
 % Transpose values into expected format
 [x2, y2] = ANNdata(x, y);
@@ -9,15 +9,17 @@ P = x2;
 % T represents the targets
 T = y2;
 
-net = feedforwardnet(54, 'traingdm');
-net = configure(net, x2, y2);
+% net = feedforwardnet(54, 'traingdm');
+% net = configure(net, x2, y2);
+% 
+% % To train the ntwork for 100 epochs
+% net.trainParam.epochs = 100;
+% net.trainParam.lr = 0.3268;
+% net.trainParam.mc = 0.5;
+% [net, tr] = train(net, P, T);
+[avgTrainingError, avgValidationError, net] = crossValidation(10, x, y, bestParams);
+avgValidationError
 
-% To train the ntwork for 100 epochs
-net.trainParam.epochs = 100;
-net.trainParam.lr = 0.3268;
-net.trainParam.mc = 0.5;
-[net, tr] = train(net, P, T);
+%validationPredictions = testANN(bestNet, x);
 
-validationPredictions = testANN(net, x);
-
-validationError = sum(validationPredictions ~= y)/size(validationPredictions,1)
+%validationError = sum(validationPredictions ~= y)/size(validationPredictions,1)
