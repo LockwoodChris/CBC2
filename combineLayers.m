af00104c7c8b5922d3b06bf23b40733be36690d9
Alvaro Sevilla <alvarosevilla95@gmail.com>
2015-12-02 11:29:39 +0000

    Added combineLayers

---
+++ combineLayers.m
@@ -0,0 +1,19 @@
+function commies = combineLayers(layers, hu)
+
+%Find the length we are going tu have in the end
+len = 0;
+for i=1:length(layers)
+    len = len + length(hu)^(layers(i));
+end
+
+%Pre-index
+commies = cell(39,1);
+index = 1;
+for i=1:length(layers)
+    m = permn(hu, layers(i));
+    for j=1:length(m)
+        commies{index} = m(j,:);
+        index = index + 1;
+    end      
+end
+end
