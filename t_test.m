680a1f245a05b1f718d91797e8f15828689be12a
Alvaro Sevilla <as11913@texel22.doc.ic.ac.uk>
2015-12-03 19:24:34 +0000

    .

--- t_test.m
+++ t_test.m
@@ -1,8 +1,17 @@
 function [] = t_test()
-%Errors for 10 fold crossvalidation
 cleanTrees = [0.237, 0.306, 0.227, 0.277, 0.217, 0.267, 0.267, 0.247, 0.227, 0.267];
-cleanNets = [0.119, 0.109, 0.119, 0.148, 0.109, 0.149, 0.149, 0.01, 0.109, 0.089];
+cleanNets  = [0.119, 0.109, 0.119, 0.148, 0.109, 0.149, 0.149, 0.01, 0.109, 0.089];
 A = cleanTrees-cleanNets;
-result = ttest(A);
-disp(result);
+[h, p] = ttest(A);
+disp(h);
+disp(p);
+
+
+noisyTrees = [0.307, 0.326, 0.406, 0.396, 0.356, 0.366, 0.376, 0.425, 0.326, 0.287];
+noisyNets  = [0.416, 0.554, 0.396, 0.327, 0.287, 0.416, 0.396, 0.366, 0.366, 0.426];
+B = noisyTrees-noisyNets;
+[h1,p1]= ttest(B);
+disp(h1);
+disp(p1);
+
 end
